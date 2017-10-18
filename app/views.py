dcc588d7073b17b398a057bd89cdeaf33f3d4764
FusionFrost <pavel-shepshelevich@yandex.ru>
2017-10-19 00:54:24 +0300

    new

--- app/views.py
+++ app/views.py
@@ -1,10 +1,10 @@
 from app import app
 import os
 import telebot
-from app.config import token
 
 
-bot = telebot.TeleBot(token)
+
+bot = telebot.TeleBot('431904557:AAHgmukJijjZs70ghCo6xj3zvnk9M6UKYD4')
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -15,14 +15,16 @@ def echo_message(message):
     bot.reply_to(message, message.text)
 
 
-@app.route("/431904557:AAHgmukJijjZs70ghCo6xj3zvnk9M6UKYD4", methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
+#@app.route("/431904557:AAHgmukJijjZs70ghCo6xj3zvnk9M6UKYD4", methods=['POST'])
+#def getMessage():
+#    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+#    return "!", 200
 
-@app.route("/")
-def webhook():
-    bot.remove_webhook()
-    URL_HOOK = "https://young-tundra-38775.herokuapp.com/"+ token
-    bot.set_webhook(url=URL_HOOK)
-    return "!", 200
+#@app.route("/")
+#def webhook():
+#    bot.remove_webhook()
+#    URL_HOOK = "https://young-tundra-38775.herokuapp.com/431904557:AAHgmukJijjZs70ghCo6xj3zvnk9M6UKYD4"
+#    bot.set_webhook(url=URL_HOOK)
+#    return "!", 200
+
+bot.polling(none_stop=True)
