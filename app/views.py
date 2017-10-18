0bc8cb5483a0c95808be5883ee005484b60b6208
FusionFrost <pavel-shepshelevich@yandex.ru>
2017-10-19 00:34:18 +0300

    new

--- app/views.py
+++ app/views.py
@@ -1,6 +1,28 @@
 from app import app
+import os
+import telebot
+from app.config import token
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+@app.route("/токен бота", methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+@app.route("/")
+def webhook():
+    bot.remove_webhook()
+    URL_HOOK = "https://young-tundra-38775.herokuapp.com/"+ token
+    bot.set_webhook(url=URL_HOOK)
+    return "!", 200
