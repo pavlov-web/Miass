95366c4ec65c02293e25e6850bc149e59f5b869e
FusionFrost <pavel-shepshelevich@yandex.ru>
2017-10-19 13:29:23 +0300

    new5

--- app/views.py
+++ app/views.py
@@ -3,9 +3,9 @@ import os
 import telebot
 from flask import request
 
+token = '431904557:AAHrVTwCYRd_eaJggx7-_lfVmgGsODrR4N0'
 
-
-bot = telebot.TeleBot('431904557:AAHrVTwCYRd_eaJggx7-_lfVmgGsODrR4N0')
+bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -16,19 +16,20 @@ def echo_message(message):
     bot.reply_to(message, message.text)
 
 
-@app.route("/431904557:AAHrVTwCYRd_eaJggx7-_lfVmgGsODrR4N0", methods=['POST'])
-def getMessage():
+
+@app.route('/' + token, methods=['POST'])
+def get_message():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
+    return "POST", 200
+
 
 @app.route("/")
-def webhook():
+def web_hook():
     bot.remove_webhook()
-    URL_HOOK = "https://young-tundra-38775.herokuapp.com/431904557:AAHrVTwCYRd_eaJggx7-_lfVmgGsODrR4N0/"
-    bot.set_webhook(url=URL_HOOK)
-    return "!", 200
+    bot.set_webhook(url='https://young-tundra-38775.herokuapp.com/' + token)
+    return "CONNECTED", 200
 
-app.run(host="0.0.0.0", port=os.environ.get('PORT', 5000), debug = True)
+app.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
 """
 #Включить, если не работают веб хуки
 bot.polling(none_stop=True)
